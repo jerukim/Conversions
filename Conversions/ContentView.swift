6d3a276cae407cce26c6b890a84b1e4d83935c09
jerumarkable <jeru@markable.ai>
2020-10-20 01:04:55 -0400

    basic setup for temperature conversions

--- Conversions/ContentView.swift
+++ Conversions/ContentView.swift
@@ -8,9 +8,84 @@
 
 import SwiftUI
 
+enum TempartureUnit: String, CaseIterable {
+    case celsius
+    case fahrenheit
+    case kelvin
+}
+
 struct ContentView: View {
+    let conversionRatesToCelsius: [String: (Double) -> Double] = [
+        "celsius": { $0 },
+        "fahrenheit": { ($0 - 32.0) * (5 / 9) },
+        "kelvin": { $0 + 273.15 }
+    ]
+    let conversionRatesFromCelsius: [String: (Double) -> Double] = [
+        "celsius": { $0 },
+        "fahrenheit": { ($0 * 9/5) + 32 },
+        "kelvin": { $0 - 273.15 }
+    ]
+    
+    @State private var initialUnit: TempartureUnit = .celsius
+    @State private var targetUnit: TempartureUnit = .fahrenheit
+    
+    @State private var initialValue: String = "0"
+    
+    private var targetValue: Double {
+        let startingValue = Double(initialValue)
+
+        if let value = startingValue {
+            let startingUnit = initialUnit.rawValue
+            
+            guard let baseConversionValue = conversionRatesToCelsius[startingUnit]?(value) else { return 0 }
+            
+            let endingUnit = targetUnit.rawValue
+
+            guard let convertedValue = conversionRatesFromCelsius[endingUnit]?(baseConversionValue) else { return 0 }
+            
+            return convertedValue
+        } else {
+            return 0
+        }
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                HStack {
+                    TextField("Convert From", text: $initialValue)
+                        .keyboardType(.decimalPad)
+                        .multilineTextAlignment(.center)
+                    
+                    Divider()
+                    
+                    Picker("From", selection: $initialUnit) {
+                        ForEach(TempartureUnit.allCases, id: \.self) {
+                            Text($0.rawValue).tag($0)
+                        }
+//                        .navigationBarTitle("Units", displayMode: .inline)
+                    }
+                }
+                
+                HStack {
+                    
+                    GeometryReader { geometry in
+                        Text("\(self.targetValue, specifier: "%.2f")")
+                            .frame(width: geometry.size.width / 2)
+                    }
+                    
+                    Divider()
+                    
+                    Picker("To", selection: $targetUnit) {
+                        ForEach(TempartureUnit.allCases, id: \.self) {
+                            Text($0.rawValue).tag($0)
+//                            .navigationBarTitle("Units", displayMode: .inline)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Conversion")
+        }
     }
 }
 
